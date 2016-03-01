8e525c9196ba64634f0fdbf446ec5d67cd62719a
Vladislav Sazanovich <zerogerc@gmail.com>
2016-03-01 11:08:49 +0300

    another write fix

--- cat/cat.c
+++ cat/cat.c
@@ -2,14 +2,22 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#define BUF_SIZE 8
+#define BUF_SIZE 1024
 
 int main() {
 	char buf[BUF_SIZE];
 	
 	int readed;
 	while ((readed = read(0, buf, BUF_SIZE)) > 0) {
-		write(1, buf, readed);
+		int writed = 0;
+		while (writed < readed) {
+			int temp = write(1, buf, readed);
+			if (temp < 0) {
+				write(2, "An error occured in the write\n", 31);
+				return 0;
+			}
+			writed += temp;
+		}
 	}
 
 	if (readed < 0) {
